6e7600307a795b26af2dda5d21ecdf7e5d21295c
safych <rs@leads.su>
2022-12-12 17:19:23 +0300

    calc game

---
+++ src/Calc.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Console\Games\Calc;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+use function Console\Games\Cli\greeting;
+
+function calcGame()
+{
+    $name = greeting();
+    line('What is the result of the expression?');
+
+    $questionsCount = 3;
+    $operators = ['+', '-', '*'];
+    $i = 0;
+    while($i < $questionsCount) {
+        $randomNumber1 = rand(2, 50);
+        $randomNumber2 = rand(2, 50);
+        $randKeysOperator = array_rand($operators, 1);
+
+        $operator = $operators[$randKeysOperator];
+        $expression = "{$randomNumber1} {$operator} {$randomNumber2}";
+        $correctAnswer = null;
+        switch ($operator) {
+            case '-':
+                $correctAnswer = $randomNumber1 - $randomNumber2;
+                break;
+            case '+':
+                $correctAnswer = $randomNumber1 + $randomNumber2;
+                break;
+            case '*':
+                $correctAnswer = $randomNumber1 * $randomNumber2;
+                break;
+        }
+
+        line("Question: " . $expression);
+        $answer = prompt("Your answer");
+
+        if ((int) $answer === $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.\nLet's try again, {$name}");
+            return;
+        }
+
+        $i += 1;
+        if ($i === $questionsCount) {
+            line("Congratulations, %s!", $name);
+        }
+    }
+}
