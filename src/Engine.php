86121cf1de1e6171a2db7f83fc1952f4b8b61b77
safych <rs@leads.su>
2022-12-14 09:08:51 +0300

    fix

--- src/Engine.php
+++ src/Engine.php
@@ -9,7 +9,7 @@ const ROUND_COUNT = 3;
 
 function greeting()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
