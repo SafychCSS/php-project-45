0f35784cfd7ca15e4e8426c99f1249504c506e4d
safych <rs@leads.su>
2022-12-14 12:34:09 +0300

    refactoring gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,42 +2,28 @@
 
 namespace Console\Games\Gcd;
 
-use function cli\line;
-use function Console\Games\Engine\congratulation;
-use function Console\Games\Engine\greeting;
-use function Console\Games\Engine\ask;
-use function Console\Games\Engine\getAnswer;
-use function Console\Games\Engine\correctAnswer;
-use function Console\Games\Engine\wrongAnswer;
+use function Console\Games\Engine\runGame;
 
 use const Console\Games\Engine\ROUND_COUNT;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 
 function gcd(int $a, int $b): int
 {
     return ($a % $b !== 0) ? gcd($b, $a % $b) : $b;
 }
 
-function gcdGame()
+function run()
 {
-    $name = greeting();
-    line('Find the greatest common divisor of given numbers.');
-
+    $gameData = [];
     $i = 0;
     while ($i < ROUND_COUNT) {
         $randomNumber1 = rand(2, 50);
         $randomNumber2 = rand(2, 50);
-        ask("{$randomNumber1} {$randomNumber2}");
-        $answer = getAnswer();
+        $question = "{$randomNumber1} {$randomNumber2}";
         $correctAnswer = gcd($randomNumber1, $randomNumber2);
-        if ($answer == $correctAnswer) {
-            correctAnswer();
-        } else {
-            wrongAnswer($answer, $correctAnswer, $name);
-            return;
-        }
         $i += 1;
-        if ($i === ROUND_COUNT) {
-            congratulation($name);
-        }
+        $gameData[] = [$question, (string) $correctAnswer];
     }
+    runGame(DESCRIPTION, $gameData);
 }
